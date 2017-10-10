e446447a4a94164004cd9a4f68531cb5b49dddd7
kuro68k <paul@world3.net>
2017-10-10 21:34:38 +0100

    Bootloader entry conditions

--- xmega_usb_bootloader/xmega_usb_bootloader/main.c
+++ xmega_usb_bootloader/xmega_usb_bootloader/main.c
@@ -5,7 +5,11 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
 #include "usb_xmega.h"
+
+typedef void (*AppPtr)(void) __attribute__ ((noreturn));
 
 extern void CCPWrite(volatile uint8_t *address, uint8_t value);
 void bootloader(void);
@@ -14,7 +18,38 @@ uint8_t page_buffer[APP_SECTION_PAGE_SIZE];
 
 int main(void)
 {
-	CCPWrite(&PMIC.CTRL, PMIC_IVSEL_bm);
+	__label__ start_bootloader;
+	
+	// entry conditions
+	if ((*(uint32_t *)(0) == 0x4c4f4144) ||				// "LOAD"
+		(*(const __flash uint16_t *)(0) == 0xFFFF))		// reset vector blank
+	{
+		*(uint32_t *)(0) = 0;							// clear signature
+		goto start_bootloader;
+	}
 
+	// SUPERPLAY buttons
+	uint8_t portb7_temp = PORTB.PIN7CTRL;
+	uint8_t portd0_temp = PORTD.PIN0CTRL;
+	PORTB.DIRCLR = PIN7_bm;					// button 1
+	PORTB.PIN7CTRL = PORT_OPC_PULLUP_gc;
+	PORTD.DIRCLR = PIN0_bm;					// meta
+	PORTD.PIN0CTRL = PORT_OPC_PULLUP_gc;
+	_delay_ms(10);
+	if (!(PORTB.IN & PIN7_bm) || !(PORTD.IN & PIN0_bm))
+		goto start_bootloader;
+	PORTB.PIN7CTRL = portb7_temp;
+	PORTD.PIN0CTRL = portd0_temp;
+
+	// exit bootloader
+	AppPtr application_vector = (AppPtr)0x000000;
+	CCP = CCP_IOREG_gc;		// unlock IVSEL
+	PMIC.CTRL = 0;			// disable interrupts, set vector table to app section
+	EIND = 0;				// indirect jumps go to app section
+	RAMPZ = 0;				// LPM uses lower 64k of flash
+	application_vector();
+	
+start_bootloader:
+	CCPWrite(&PMIC.CTRL, PMIC_IVSEL_bm);
 	bootloader();
 }
